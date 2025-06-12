a0acb5100cc0b4c9ad73bfd00fa3155a196b4b79
Zymantas Katinas <zymantas.katinas@decodo.com>
2025-06-12 12:20:32 +0300

    Include favicon

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -52,6 +52,25 @@ export const metadata: Metadata = {
   authors: [{ name: "CommitDigest Team" }],
   creator: "CommitDigest",
   publisher: "CommitDigest",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      {
+        url: "/favicon/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    other: [
+      {
+        rel: "mask-icon",
+        url: "/favicon/safari-pinned-tab.svg",
+      },
+    ],
+  },
   openGraph: {
     type: "website",
     locale: "en_US",
